5082113039c54e50137a875e4ebabd64bcd0031d
haeunNoh06 <okhagrace06@naver.com>
2023-03-14 15:08:33 +0900

    유식하게 구구단

--- JWP/gugudan.cpp
+++ JWP/gugudan.cpp
@@ -3,16 +3,11 @@
 int main(void) {
 
 	for (int i = 2; i <= 9; i++) {
-		printf("%d x 1 = %d\n", i, i * 1);
-		printf("%d x 2 = %d\n", i, i * 2);
-		printf("%d x 3 = %d\n", i, i * 3);
-		printf("%d x 4 = %d\n", i, i * 4);
-		printf("%d x 5 = %d\n", i, i * 5);
-		printf("%d x 6 = %d\n", i, i * 6);
-		printf("%d x 7 = %d\n", i, i * 7);
-		printf("%d x 8 = %d\n", i, i * 8);
-		printf("%d x 9 = %d\n", i, i * 9);
+		for (int j = 1; j <= 9; j++) {
+			printf("%d x%d = %d\n", i, j, i * j);
+		}
+		printf("\n");
 	}
 
 	return 0;
-}
+} 

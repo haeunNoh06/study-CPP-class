675701ca73e0bebc17065ffd7d75093ec49f93f9
haeunNoh06 <okhagrace06@naver.com>
2023-03-17 09:50:36 +0900

    cpp 변수 출력

--- JWP/grammer/C++기본.cpp
+++ JWP/grammer/C++기본.cpp
@@ -1,11 +1,17 @@
 ﻿// cout 객체를 사용하기 위해 iostream을 불러옴
 #include <iostream>
 
+// 이름 공간
+using namespace std;
+
 int main(void) {
+
+	int year = 2023;
+	char h = 'd';
 
 	// << : 출력 연산자
 	// endl : 개행
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World " << year << endl;
 
 	return 0;
 }

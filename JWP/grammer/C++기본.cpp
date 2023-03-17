fc9b7bcf097e890971dd6584fe55fd6a4290eff0
haeunNoh06 <okhagrace06@naver.com>
2023-03-17 09:15:22 +0900

    C++ style 1

--- JWP/grammer/C++기본.cpp
+++ JWP/grammer/C++기본.cpp
@@ -1,8 +1,10 @@
-﻿#include <stdio.h>
+﻿// cout 객체를 사용하기 위해 iostream을 불러옴
+#include <iostream>
 
 int main(void) {
 
-	printf("Hello World");
+	// << 는 출력 연산자
+	std::cout << "Hello World\n";
 
 	return 0;
 }

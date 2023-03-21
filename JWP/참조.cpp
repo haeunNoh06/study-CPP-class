6b18acc90d6b6a89b334df3ffd2adc2c8949da30
haeunNoh06 <okhagrace06@naver.com>
2023-03-21 13:56:31 +0900

    참조(reference)

--- JWP/참조.cpp
+++ JWP/참조.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main(void) {
-	int a = 30;	// a : 30
-	int* pa = &a; // pa : a의 주소값
+	int a = 30;
+	int& ra = a; 
 
 
-	*pa = 100;	// a : 100
+	ra = 100;	// a : 100
 	cout << "a의 값은 " << a << endl;
 
 	return 0;

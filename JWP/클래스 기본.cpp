06059e9b5e637e46ab486145336d76697cf906c0
haeunNoh06 <okhagrace06@naver.com>
2023-03-31 09:52:42 +0900

    class, 접근 제어자 public

--- JWP/클래스 기본.cpp
+++ JWP/클래스 기본.cpp
@@ -6,9 +6,9 @@ using namespace std;
 // 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
 // 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 함
 struct Student {
-	// 성능때문에 id를 문자열로 하지 않음
-	// 일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요구하며
-	// 정수는 비교 연산을 한 번에 할 수 있는데 문자열은 글자수 만큼 반복하여 비교해야함
+	// struct는 public이 default
+	// class는 private이 default
+public:
 	int id;
 	string name;
 	string tel;
@@ -18,7 +18,7 @@ struct Student {
 
 int main(void) {
 
-	// 자료형 : struct Student ( 하지만 C++에서는 Student만 써도 가능 )
+	// 자료형 : Student ( 클래스 생략 가능 )
 	struct Student student;
 	student.id = 3434;
 	student.name = "김길동";

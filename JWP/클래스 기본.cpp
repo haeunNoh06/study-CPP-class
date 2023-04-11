01afdfcc4c0eff9010377115ccc3b4656db84c94
haeunNoh06 <okhagrace06@naver.com>
2023-04-11 14:54:31 +0900

    setter

--- JWP/클래스 기본.cpp
+++ JWP/클래스 기본.cpp
@@ -17,31 +17,33 @@ public:
     //생성자 (constructor) : 객체를 생성할 때 호출되는 함수
     // 생성자는 반환형을 쓰지 않는다.
     //생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
-    int studentId;
-    string name;
-    string telephone;
-    string department;
-    string address;
-    Student() {
-        address = "김포";
-        department = "소프트웨어과";
-        name = "김씨";
-        studentId = 2211;
-        telephone = "123-4567-8912";
+    Student() {}
 
-    }
     Student(int studentId, string name, string telephone, string department, string address) 
         // 멤버변수 초기화. const/참조형 멤버변수 사용가능
         : studentId(studentId), name(name), telephone(telephone), department(department), address(address)
     {
         
     }
+
+    void set_studentId(int _studentId) { studentId = _studentId; }
+    void set_name(string _name) { name = _name; }
+    void set_telephone(string _telephone) { telephone = _telephone; }
+    void set_department(string _department) { department = _department; }
+    void set_address(string _address) { address = _address; }
+
     // class 멤버 함수를 가질 수 있다
     void print(void) {
         cout << "이름 : " << name << "\n과 : " << department << "\n학번 : " << studentId <<
             "\n전화번호 : " << telephone << "\n집주소 : " << address << endl;
     }
 
+private:
+    int studentId;
+    string name;
+    string telephone;
+    string department;
+    string address;
 
 };
 
@@ -55,10 +57,10 @@ int main(void) {
     //kim2.print();
 
 
-    Student stu3[2];
+    /*Student stu3[2];
     for (int i = 0; i < sizeof(stu3)/sizeof(stu3[0]); i++) {
         stu3[i].print();
-    }
+    }*/
 
 
     // 동적할당 : 실행시간(runtime)에 메모리 크기가 결정(메모리 heap영역)
@@ -67,12 +69,27 @@ int main(void) {
     //stu3->print();
     //// 동적할당 해제(안 하면 메모리 누수현상 발생)
     //delete stu3;
-
-
+    
     Student* stu4 = new Student[2];
-    for (int i = 0; i < sizeof(stu4) / sizeof(stu4[0]); i++) {
+
+    stu4[0].set_studentId(2030);
+    stu4[0].set_name("길동이");
+    stu4[0].set_telephone("010-1234-1234");
+    stu4[0].set_department("항공과");
+    stu4[0].set_address("서울시");
+    
+    
+    
+    stu4[1].set_studentId(2033);
+    stu4[1].set_name("춘향이");
+    stu4[1].set_telephone("010-4444-777");
+    stu4[1].set_department("디자인과");
+    stu4[1].set_address("충남");
+
+    for (int i = 0; i < 2; i++) {
         stu4[i].print();    // 배열의 요소에 해당하는 객체는 멤버를 .으로 접근
     }
+
 
     delete[] stu4;
 

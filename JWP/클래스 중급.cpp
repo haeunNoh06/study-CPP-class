487d736c991815e7fb3d8fd2c3ad869031a3c94a
haeunNoh06 <okhagrace06@naver.com>
2023-04-18 14:54:56 +0900

    멤버변수 동적할당

--- JWP/클래스 중급.cpp
+++ JWP/클래스 중급.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -6,15 +7,16 @@ class MString {
 public:
 	MString(const char* str)
 	{ // !주의! const를 입력해야 됨
-		c_str_ = str;
+		unsigned int l = strlen(str);
+		c_str_ = new char[l + 1];	// '\0'문자가 들어갈 공간 (+1)
+		strcpy(c_str_, str);	// c_str_에 str을 복사한다.
+		size_ = l;
 	}
-	void set_size(int size) { size_ = size; }
-	void set_c_str(const char* c_str) { c_str_ = c_str; }
 	unsigned int size(void) { return size_; }
-	const char* c_str(void) { return c_str_;  }
+	char* c_str(void) { return c_str_;  }
 private: 
 	unsigned int size_;	// 문자열의 길이
-	const char* c_str_;	// 문자열의 시작 주소
+	char* c_str_;	// 문자열의 시작 주소
 };
 
 int main(void) {

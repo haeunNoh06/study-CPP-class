a5dc24b1232254ced774cf1dd4d695376b5c57a5
haeunNoh06 <okhagrace06@naver.com>
2023-04-25 13:44:23 +0900

    생성자, 소멸자 console로 확인

--- JWP/클래스 중급.cpp
+++ JWP/클래스 중급.cpp
@@ -11,12 +11,14 @@ public:
 		c_str_ = new char[l + 1];	// '\0'문자가 들어갈 공간 (+1)
 		strcpy(c_str_, str);	// c_str_에 str을 복사한다.
 		size_ = l;
+		cout << "MString 생성자 호출 완료" << endl;
 	}
 
 	// 소멸자의 매개변수는 없다.
 	~MString() {
 		// 소멸자로 생성자에서 동적할당한 메모리 해제
 		delete[] c_str_;
+		cout << "MString 소멸자 호출 완료" << endl;
 	}
 
 	unsigned int size(void) { return size_; }

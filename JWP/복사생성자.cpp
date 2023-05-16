2d1858f0a6d5c03bff82c6842f1c47a0ac89001e
haeunNoh06 <okhagrace06@naver.com>
2023-05-16 14:27:49 +0900

    복사생성자(얕은 복사)

--- JWP/복사생성자.cpp
+++ JWP/복사생성자.cpp
@@ -5,11 +5,17 @@ using namespace std;
 
 class Munja {
 public:
+	// 일반생성자
 	Munja(const char* jhqz) {
 		// null문자가 있기 때문에 +1을 해주는 것 -> 3("abc")+1(null문자)
 		str_ = new char[strlen(jhqz) + 1];
 		strcpy(str_, jhqz);// str_과 jhqz를 서로 바꾼다.
 		cout << "일반 생성자 호출" << endl;
+	}
+	// 복사생성자(얕은복사) 오류발생
+	Munja(const Munja& hj) : str_(hj.str_)
+	{
+		cout << "복사생성자 호출" << endl;
 	}
 	~Munja() {
 		delete[] str_;
@@ -20,6 +26,8 @@ private:
 };
 
 int main(void) {
-	Munja m1 = Munja("abc");
+	// 객체 두 개, 생성자 두 번 호출.
+	Munja m1 = Munja("abc");// 일반생성자 호출
+	Munja m2 = m1;			// 복사생성자 호출
 	return 0;
 }

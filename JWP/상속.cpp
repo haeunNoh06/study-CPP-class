984353e6d739717c89ef853847dfdcce257cf8a2
haeunNoh06 <okhagrace06@naver.com>
2023-06-09 09:45:56 +0900

    [상속] 부모의 멤버변수/함수를 사용할 수 있다

--- JWP/상속.cpp
+++ JWP/상속.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 class Animal {
 public:
-	Animal() { cout << "동물 생성자" << endl; }
+	Animal(int age, string name) : age_(age), name_(name)
+	{ 
+		cout << "동물 생성자" << endl;
+		cout << "나이 " << age_ << endl;
+		cout << "이름 " << name_ << endl;
+	}
 	~Animal() { cout << "동물 소멸자" << endl; }
 	void Bark(void) { cout << "동물 짖는다." << endl; }
 	void Eat(void) { cout << "동물 먹는다." << endl; }
@@ -18,7 +23,12 @@ private:
 class Crane : public Animal {
 public:
 	// 별도의 언급이 없어도 매개변수가 없는 부모생성자가 호출된다.
-	Crane() { cout << "두루미 생성자" << endl; }
+	Crane(int age, string name, int leg_length) : Animal(age, name)
+	{ 
+		cout << "두루미 생성자" << endl; 
+		leg_length_ = leg_length;
+		cout << "두루미 다리길이 " << leg_length_ << endl;
+	}
 	~Crane() { cout << "두루미 소멸자" << endl; }
 
 private:
@@ -27,7 +37,8 @@ private:
 };
 
 int main(void) {
-	Crane* crane = new Crane();
+	Crane* crane = new Crane(3,"지우", 108);	// 부모의 멤버변수를 사용
+	crane->Eat(); // 부모의 멤버함수를 사용
 
 	delete crane;
 

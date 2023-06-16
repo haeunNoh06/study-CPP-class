9b8585854ce1ba9754b2290beb99552b81120b7d
haeunNoh06 <okhagrace06@naver.com>
2023-06-16 09:43:39 +0900

    [상속] 소멸자에는 반드시 virtual을 넣자

--- JWP/상속.cpp
+++ JWP/상속.cpp
@@ -7,13 +7,15 @@ public:
 	Animal(int age, string name) : age_(age), name_(name)
 	{ 
 		cout << "동물 생성자" << endl;
-		cout << "나이 " << age_ << endl;
-		cout << "이름 " << name_ << endl;
 	}
-	~Animal() { cout << "동물 소멸자" << endl; }
-	void Bark(void) { cout << "동물 짖는다." << endl; }
-	void Eat(void) { cout << "동물 먹는다." << endl; }
-	void Hunt(void) { cout << "동물 사냥한다." << endl; }
+
+	// 소멸자에는 무조건 virtual을 넣자(메모리 누수 방지)
+	virtual ~Animal() { cout << "동물 소멸자" << endl; }
+
+	// 동적 바인딩(가상함수)
+	virtual void Bark(void) { cout << "동물 짖는다." << endl; }
+	virtual void Eat(void) { cout << "동물 먹는다." << endl; }
+	virtual void Hunt(void) { cout << "동물 사냥한다." << endl; }
 private:
 	int age_;
 	string name_;
@@ -27,20 +29,34 @@ public:
 	{ 
 		cout << "두루미 생성자" << endl; 
 		leg_length_ = leg_length;
-		cout << "두루미 다리길이 " << leg_length_ << endl;
 	}
-	~Crane() { cout << "두루미 소멸자" << endl; }
+	virtual ~Crane() { cout << "두루미 소멸자" << endl; }
+
+	// 오타같은 실수를 방지하기 위해 override 키워드 사용 (Java의 @Override)
+	void Bark() override {
+		cout << "두루두루" << endl;
+	}
 
 private:
 	int leg_length_;
-
 };
 
 int main(void) {
-	Crane* crane = new Crane(3,"지우", 108);	// 부모의 멤버변수를 사용
-	crane->Eat(); // 부모의 멤버함수를 사용
 
-	delete crane;
+	// Crane c = Crane(1, "1", 1);
+	// c.Bark();
+
+	Animal* animal = new Animal(18, "동물이");
+	animal->Bark();	// 두루두루
+	delete animal;// 포인터가 지워지는 게 아님
+
+	// animal = new Crane(3, "지우", 108);
+	// animal->Bark();	// 동물 짖는다(정적 바인딩으로 인해 부모의 멤버함수를 호출)
+	// delete animal;
+
+	//Crane* crane = new Crane(3,"지우", 108);	// 부모의 멤버변수를 사용
+	//crane->Eat(); // 부모의 멤버함수를 사용
+	//delete crane;
 
 	return 0;
 }
